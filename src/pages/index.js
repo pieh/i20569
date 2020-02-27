f69463a398434ce40ee6aae342ddbc94048da627
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-02-27 20:23:13 +0100

    put image way below the fold

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <hr style={{ marginBottom: `300vh` }} />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
